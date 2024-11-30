853aacad6e26f3ba3684339f48d2c8f3211fb365
oscar05 <santiagoM0509@hotmail.com>
2024-11-29 20:25:23 -0500

    fix bug DataType

--- api/db/migrations/20241128120917-add-role.js
+++ api/db/migrations/20241128120917-add-role.js
@@ -3,8 +3,6 @@
 const { USER_TABLE } = require('./../models/user.model');
 const { Model, DataTypes, Sequelize } = require('sequelize');
 
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
     await queryInterface.addColumn(USER_TABLE, 'role', {
